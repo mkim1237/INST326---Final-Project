4d9321eef68eb637c7c25766403536554b465106
Min Kim <mkim1237@umd.edu>
2022-11-16 21:12:19 -0500

    Wrote some basic ideas of our code

--- final_project.py
+++ final_project.py
@@ -1,12 +1,68 @@
-""" Let's Do This :) 
-    We got this """
+#Min: Let's Do This :) We got this
+#Bushrah: Hello Hello, All
+#Kingsley: Hello world
+#Shannon: Hi team!
+#James: Hi Everyoneeoa
+#
+#
 
-Hello
-=======
-Hello, All
 
-Hello world
+#class Contact:
+    """ Reads a line of contact list and divides the contact info into different 
+    attributes.
+    
+    """
+    #def __init__():
+        """ reads the line and use regular expression to initialize contact values 
+        (names, phone number, relation, etdc)
+        
+        """
+    
+    #def __repr__():
+        """ magic method that return formal representation of contact object
+        
+        """
+    
 
-Hi team!
+#def readCont():
+    """ Using with open, read all lines of contact and puts them into a list
+    
+    """
 
-Hi Everyoneeoa
+#class Sort:
+    """ Put the list of contacts into a dataframe using panda. Then, sort them 
+    to put different groups/cateogries of the contact into separate dataframes.
+        
+    """
+    #def __init__()
+    
+    
+    #def
+
+#class Find(Contact):
+    """ Finds the person or category
+    
+    """
+    #def __init__():
+        """ use super() ..?? to initialize?
+        
+        """
+    
+    #def choose():
+        """
+        
+        """
+
+#def Msg():
+    """ use conditional expressions to send a message
+    
+    """
+
+#def main():
+    """ has optional parameters
+    
+    """
+
+#if __name__ == "__main__":
+
+
