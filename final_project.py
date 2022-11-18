11b6ec29d0c2e4b44fcf7f0f13b0f23e2d8f8543
Min Kim <mkim1237@umd.edu>
2022-11-18 00:29:13 -0500

    wrote docstrings

--- final_project.py
+++ final_project.py
@@ -7,7 +7,7 @@
 #
 
 
-#class Contact:
+class Contact:
     """ Reads a line of contact list and divides the contact info into different 
     attributes.
     
@@ -15,44 +15,64 @@
     def __init__(self, name, address, email, phoneNum):
         """ reads the line and use regular expression to initialize contact values 
         (names, phone number, relation, etdc)
+        
         Args: 
-        name(str): the name of the person in contactlist
-        address(str): the address of the person in contactlist
-        email(str): the email of the person in contactlist
-        phoneNum(int): the phoneNum of the person in contactlist
+            name(str): the name of the person in contactlist
+            address(str): the address of the person in contactlist
+            email(str): the email of the person in contactlist
+            phoneNum(int): the phoneNum of the person in contactlist
         
         """
         
 
     
-        
-    
     def __repr__(self):
         """ magic method that return formal representation of contact object
+        
+        
         Returns:
         returns self of the name, address, email and phoneNum
         
         """
-        
-        
     
 
-#def readCont():
-    """ Using with open, read all lines of contact and puts them into a list
+    
+
+def readCont(filename):
+    """ Using with open, read all lines of contact file and puts them into a single list.
+    
+    Args:
+        filename (str): the text filepath of the lines of contact info.
+        
+    Returns:
+        contactList (list of str): a list of each line that is the Contact object.
     
     """
 
-#class Sort:
-    """ Put the list of contacts into a dataframe using panda. Then, sort them 
+class Sort:
+    """ Convert the list of contacts into a dataframe using panda. Then, sort them 
     to put different groups/cateogries of the contact into separate dataframes.
         
     """
-    #def __init__()
+    def __init__(self, list):
+        """ Creates an attribute of dataframe by converting the list argument.
+        
+        Args:
+            list (list of str): the list of contact information made by function 
+            readCont
+        
+        Side effects:
+            contDf (dataframe): dataframe of the contact
+        
+        """
     
+    def categorize():
+        """
+        
+        """
     
-    #def
 
-#class Find(Contact):
+#class Find(Sort):
     """ Finds the person or category
     
     """
@@ -70,19 +90,15 @@
     """ use conditional expressions to send a message
     
     """
+    
+    
 
 #def main():
     """ has optional parameters
     
     """
 
+
 #if __name__ == "__main__":
 
 
-<<<<<<< HEAD
-=======
-Hi Everyoneeoa 
-
-Testing
-testing again to see something...
->>>>>>> c1ee50c804d3a12fffb76da37ced4b5586660682
